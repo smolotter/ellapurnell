4b2a00d2996acacdae6fa95983d70ba3068b3691
smolotter <86969144+smolotter@users.noreply.github.com>
2024-06-13 15:40:42 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit
 from seleniumprint import file_to_pdf
 
-input_html_file_path="/Users/user/path/to/input.html"
-output_pdf_file_path="/Users/user/path/to/output.pdf"
+# input_html_file_path="/Users/user/path/to/input.html"
+# output_pdf_file_path="/Users/user/path/to/output.pdf"
 
-file_to_pdf(input_html_file_path, output_pdf_file_path)
+# file_to_pdf(input_html_file_path, output_pdf_file_path)
 
-input_html_url="http://localhost:8000/report/1"
-output_pdf_file_path="./report_pdfs/report_1.pdf"
+# input_html_url="http://localhost:8000/report/1"
+# output_pdf_file_path="./report_pdfs/report_1.pdf"
 
-url_to_pdf(input_html_url, output_pdf_file_path)
+# url_to_pdf(input_html_url, output_pdf_file_path)
