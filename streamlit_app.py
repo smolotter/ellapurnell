ef12f6853d16aaaadf4b2170fc35c178a24d6b72
smolotter <86969144+smolotter@users.noreply.github.com>
2024-06-13 16:04:03 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,45 @@
 import streamlit as st
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
+# with st.echo():
+#     from selenium import webdriver
+#     from selenium.webdriver.chrome.options import Options
+#     from selenium.webdriver.chrome.service import Service
+#     from webdriver_manager.chrome import ChromeDriverManager
+#     from webdriver_manager.core.os_manager import ChromeType
 
-    options = Options()
-    options.add_argument("--disable-gpu")
-    options.add_argument("--headless")
+#     @st.cache_resource
+#     def get_driver():
+#         return webdriver.Chrome(
+#             service=Service(
+#                 ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+#             ),
+#             options=options,
+#         )
 
-    driver = get_driver()
-    driver.get("http://example.com")
+#     options = Options()
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--headless")
 
-    st.code(driver.page_source)
+#     driver = get_driver()
+#     driver.get("http://example.com")
+
+#     st.code(driver.page_source)
+
+
+
+
+
+
+
+import os
+from pyhtml2pdf import converter
+# path = os.path.abspath('index.html')
+# converter.convert(f'file:///{path}', 'sample.pdf') #local file
+
+converter.convert('https://pypi.org', 'sample.pdf') #web based
+
+
+with open("sample.pdf", "rb") as pdf_file:
+    data = pdf_file.read()
+st.download_button("Download sample.pdf", data=data, file_name="sample.pdf")
