e681b26299210039e7a027678047ed88bf757cf3
smolotter <86969144+smolotter@users.noreply.github.com>
2024-06-14 05:24:49 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,24 +42,6 @@ def html_to_pdf(html_file_path):
 #     # Offer download link for the PDF
 #     st.download_button(label="Download PDF", data=open(pdf_path, 'rb').read(), file_name="converted.pdf")
 
-def html_to_pdf(html_file_path):
-    pdf_file = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
-    pdf_file.close()
-
-    command = [
-        "google-chrome-stable",
-        "--headless",
-        "--no-sandbox",
-        "--disable-gpu",
-        "--print-to-pdf=" + pdf_file.name,
-        html_file_path,
-     ]
-
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    process.communicate()
-
-    return pdf_file.name
-
 st.title("ZIP to PDF Converter")
 
 uploaded_zip = st.file_uploader("Upload ZIP file")
