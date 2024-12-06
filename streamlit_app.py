c7975f87a621865a9119cae049d4b350aae8fb05
smolotter <86969144+smolotter@users.noreply.github.com>
2024-12-06 17:50:12 +0000

    debg

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,10 +102,10 @@ def main():
                     html_to_pdf(index_html, hc_pdf)
                     pdf_files_hc.append(hc_pdf)
 
-                    smc_index_html = temp_dir_num + "/SMC_index.html" 
-                    smc_pdf = temp_dir_pdf + "/" +  str(i + 1) + "_smc.pdf"
-                    html_to_pdf(smc_index_html, smc_pdf)
-                    pdf_files_smc.append(smc_pdf)
+                    # smc_index_html = temp_dir_num + "/SMC_index.html" 
+                    # smc_pdf = temp_dir_pdf + "/" +  str(i + 1) + "_smc.pdf"
+                    # html_to_pdf(smc_index_html, smc_pdf)
+                    # pdf_files_smc.append(smc_pdf)
                     
             # st.write (pdf_files_hc)
             
