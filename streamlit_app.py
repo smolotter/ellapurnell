6e3d6319b927689a65cfabe59d926c96cca0436a
smolotter <86969144+smolotter@users.noreply.github.com>
2024-12-06 15:24:33 +0000

    expander with examples

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,15 @@ def main():
     st.title("Dynamic File Uploader")
 
     # Create a dropdown menu with options from 1 to 50
-    num_files = st.selectbox("Select Number of Files", range(1, 101))
+    st.markdown("**How many zip files do you have?**")
+    with st.expander("Click here to see examples")    
+        st.write("The minimum is 2: One product body + One distribution list.")
+        st.write("If there is an annex then 3: One product body + One annex + One distribution list.")
+        st.write("If there is a covernote but no annex, also 3: One covernote + One product body + One distribution list.")
+        st.write("If there is a covernote and an annex, then 4: One covernote + One product body + One annex + One distribution list.")
+        st.write("If this is a ES package, you may need more. For example, One packaging note + One EN + One distribution + Five Pensketches + Five distribution lists = 13.")
+        
+    num_files = st.selectbox("Select Number of Files", range(2, 101))
 
     # Create a list to store the uploaded files
     uploaded_files = []
